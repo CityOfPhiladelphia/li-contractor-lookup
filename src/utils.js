54221f01fbab3e2f37fbbdac2a8f7d180f32e269
Philip Ribbens <Philip.Ribbens@phila.gov>
2020-03-25 16:45:52 -0400

    Small fix

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
 export const DATE_FORMAT = "M-DD-YYYY";
 export const BASE_REST_SERVICE_URL =
-  "https://services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/ContractorLookupApplicationEclipse/FeatureServer/0/query?";
+  "https://services.arcgis.com/fLeGjb7u4uXqeF9q/ArcGIS/rest/services/ContractorLookupApplicationEclipse/FeatureServer/0/query?";
